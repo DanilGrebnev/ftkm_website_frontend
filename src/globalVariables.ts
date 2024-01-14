e01f32116306833885de538f1529af2f9f7bd7fb
Danil_Grebnev <t902hda@mail.ru>
2024-01-15 02:03:22 +0300

    refactoring

--- src/globalVariables.ts
+++ src/globalVariables.ts
@@ -1,22 +1,5 @@
 export const globalVariables = {
-    get API_PORT() {
-        return 8089
-    },
-
-    get URL_PRODUCTION() {
-        return `https://vstu.mitlp.ru/`
-    },
-
-    get URL_DEVELOPMENT() {
-        return `http://localhost:${this.API_PORT}/`
-    },
-
-    //URL обращения к API
-    get URL() {
-        return this.URL_DEVELOPMENT
-    },
-
     get limit() {
-        return 5
+        return 8
     },
 }
